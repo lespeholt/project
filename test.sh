8a35e3f816bf5ac36e409e738f4c93e0d517355f
Lasse Espeholt <lasse@espeholt.dk>
2014-08-16 19:34:34 -0700

    Fixed test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-/usr/local/bin/g++-4.8 -Wall -Wa,-q -std=c++11 src/gtest.cpp src/*_test.cpp -I src/ -lGtest -lGtest_main -DGTEST_USE_OWN_TR1_TUPLE=1 -o test.out
-./test.out
+if /usr/local/bin/g++-4.8 -Wall -Wa,-q -std=c++11 src/gtest.cpp src/*_test.cpp -I src/ -lGtest -lGtest_main -DGTEST_USE_OWN_TR1_TUPLE=1 -o test.out ; then
+	./test.out
+fi
